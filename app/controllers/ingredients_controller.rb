0b86dfa2907c0e4ab242a399c5683eccce0db845
Issa sangare <issabmsangare@hotmail.com>
2020-06-29 18:13:52 -0400

    added views for users and ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,10 @@
 class IngredientsController < ApplicationController
+
+    def index
+        @ingredients = Ingredient.left_joins(:allergies).group(:id).order("COUNT(user_id) DESC")
+    end
+
+    def show
+
+    end
 end
