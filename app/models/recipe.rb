baed360003485858ae2aac353068c7fd3b4a0eaa
Issa sangare <issabmsangare@hotmail.com>
2020-06-29 14:03:24 -0400

    initial commit

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
   has_many :ingredients_recipes
   has_many :ingredients, through: :ingredients_recipes
+
+  def ingredient_count
+    self.ingredients.count
+  end
+
 end
